64ca6be67c77c044f04c3d16ff8bec07ca22c5f7
Yoshi.dev <fujitaniyoshilynp@gmail.com>
2023-11-01 09:21:56 +0800

    Added: Logout function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,13 @@ import { useGetUsers } from "./userGetUsers";
 import { useUser } from "../pages/auth/useUser";
 import { useState } from "react";
 import { uploadAvatar } from "../services/apiUser";
+import { useLogOut } from "./useLogOut";
 
 const Navbar = () => {
   const { isAuthenticated, user } = useUser();
   const { userInfo, isLoading: gettingData } = useGetUsers();
   const [selectedImage, setSelectedImage] = useState(null);
+  const { logout, isLoading: isLoggingOut } = useLogOut();
 
   const currentUserInfo = userInfo?.find((users) => users.id === user.id);
 
@@ -38,14 +40,17 @@ const Navbar = () => {
         </div>
         {isAuthenticated && (
           <div className="">
-            <button>LogOut</button>
+            <button onClick={logout} disabled={isLoggingOut}>
+              LogOut
+            </button>
           </div>
         )}
       </div>
-      <div className="">
+      {/* Avatar upload */}
+      {/* <div className="">
         <input type="file" accept="image/*" onChange={handleImageChange} />
         <button onClick={handleUploadAvatar}>Upload Avatar</button>
-      </div>
+      </div> */}
     </nav>
   );
 };
---
+++ src/components/useLogOut.js
@@ -0,0 +1,21 @@
+import { useMutation } from "@tanstack/react-query";
+import { logout as LogOutAPI } from "../services/apiAuth";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+export function useLogOut() {
+  const navigate = useNavigate();
+  const { mutate: logout, isLoading } = useMutation({
+    mutationFn: LogOutAPI,
+
+    onSuccess: () => {
+      toast.success("Succesfully Logged Out");
+      navigate("/login");
+    },
+    onError: (err) => {
+      toast.error(err.message);
+    },
+  });
+
+  return { logout, isLoading };
+}
